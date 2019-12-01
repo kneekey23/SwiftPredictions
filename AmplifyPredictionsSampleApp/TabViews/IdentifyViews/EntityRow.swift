5461783d1af1d96d6c314ee3226b4198885614c3
Nicki Stone <nickik@amazon.com>
2019-12-01 14:18:24 -0800

    more changes

--- AmplifyPredictionsSampleApp/TabViews/IdentifyViews/EntityRow.swift
+++ AmplifyPredictionsSampleApp/TabViews/IdentifyViews/EntityRow.swift
@@ -16,9 +16,9 @@ struct EntityRow: View {
     var body: some View {
         NavigationLink(destination: EntityDetailView(incomingImage: image, entity: entity)) {
             HStack {
-                Text("Gender: " + entity.gender.gender.rawValue)
+                Text("Gender: " + (entity.gender?.gender.rawValue ?? "N/A"))
                 Spacer()
-                Text("Age: " + String(entity.ageRange.low) + " - " + String(entity.ageRange.high))
+                Text("Age: " + String(entity.ageRange?.low ?? 0)  + " - " + String(entity.ageRange?.high ?? 0))
             }
         }
     }
